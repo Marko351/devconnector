f357eb2c5085721ce350ce4d63acc79b6e72fbff
Marko351 <markodjuric351@gmail.com>
2019-02-22 13:59:35 +0100

    Finishing up User API routes and JWT Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://marko351:marko351@cluster0-shard-00-00-eltje.mongodb.net:27017,cluster0-shard-00-01-eltje.mongodb.net:27017,cluster0-shard-00-02-eltje.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+  mongoURI: 'mongodb://marko351:marko351@cluster0-shard-00-00-eltje.mongodb.net:27017,cluster0-shard-00-01-eltje.mongodb.net:27017,cluster0-shard-00-02-eltje.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'secret'
 }
